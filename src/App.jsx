478a48a95edf1f89caf1f4f63f3c4603cf7c372f
Diana Selyuk <selukdiana@gmail.com>
2025-04-18 19:17:37 +0300

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { Routes, Route } from "react-router";
 import { Header } from "./layout/Header";
 import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
 
 const App = () => {
   return (
-    <div>
-      {/* <Header />
-      <HomePage /> */}
-      <LoginPage />
-    </div>
+    <Routes>
+      <Route path="login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <HomePage />
+          </>
+        }
+      />
+    </Routes>
   );
 };
 
