8f84b762b31a4eb498e9a0085c1e4097173cf67a
Diana Selyuk <selukdiana@gmail.com>
2025-04-24 22:45:53 +0300

    feat: finish unit tests for auth

--- client/src/store/tests/authReducer.test.js
+++ client/src/store/tests/authReducer.test.js
@@ -1,14 +1,19 @@
-import authReducer, { loginUser } from "../slices/authSlice";
+import authReducer, { loginUser } from '../slices/authSlice'
 
 const initialState = {
   status: null,
 }
-
-
-
 describe('authSlice', () => {
-  it.todo('should change status with "auth.pending" action');
-  it.todo('should change status with "auth.fulfilled" action');
-  it.todo('should change status with "auth.rejected" action');
-
+  test('should change status with "auth.pending" action', () => {
+    const state = authReducer(initialState, loginUser.pending())
+    expect(state.status).toBe('pending')
+  })
+  test('should change status with "auth.fulfilled" action', () => {
+    const state = authReducer(initialState, loginUser.fulfilled())
+    expect(state.status).toBe('authorized')
+  })
+  test('should change status with "auth.rejected" action', () => {
+    const state = authReducer(initialState, loginUser.rejected())
+    expect(state.status).toBe('unauthorized')
+  })
 })
--- client/src/store/tests/authThunk.test.js
+++ client/src/store/tests/authThunk.test.js
@@ -1,47 +1,39 @@
-import { loginUser } from "../slices/authSlice";
+import { loginUser } from '../slices/authSlice'
 
-global.fetch = jest.fn();
+global.fetch = jest.fn()
 describe('authThunk', () => {
   test('should fetch with resolved response', async () => {
     const userData = {
       name: 'admin',
-      password: '1234'
+      password: '1234',
     }
-    const answer = true;
+    const answer = true
     fetch.mockResolvedValue({
-      ok: true,
-      json: () => Promise.resolve(answer)
+      json: () => Promise.resolve(answer),
     })
-    const dispatch = jest.fn();
-    const thunk = loginUser(userData);
+    const dispatch = jest.fn()
+    const thunk = loginUser(userData)
     await thunk(dispatch)
-
-    const { calls } = dispatch.mock;
-    console.log(calls)
-    expect(calls).toHaveLength(2);
-    const [start, end] = calls;
+    const { calls } = dispatch.mock
+    expect(calls).toHaveLength(2)
+    const [start, end] = calls
     expect(start[0].type).toBe(loginUser.pending().type)
     expect(end[0].type).toBe(loginUser.fulfilled().type)
     expect(end[0].payload).toBe(true)
-  });
+  })
   test('should fetch with rejected response', async () => {
     const userData = {
       name: 'aaaa',
-      password: 'bbbb'
+      password: 'bbbb',
     }
-    fetch.mockResolvedValue({
-      ok: false,
-    })
-    const dispatch = jest.fn();
-    const thunk = loginUser(userData);
-    await thunk(dispatch, () => ({}))
-
-    const { calls } = dispatch.mock;
-    console.log(calls)
-    expect(calls).toHaveLength(2);
-    const [start, end] = calls;
+    fetch.mockRejectedValue(new Error("Can't fetch"))
+    const dispatch = jest.fn()
+    const thunk = loginUser(userData)
+    await thunk(dispatch)
+    const { calls } = dispatch.mock
+    expect(calls).toHaveLength(2)
+    const [start, end] = calls
     expect(start[0].type).toBe(loginUser.pending().type)
     expect(end[0].type).toBe(loginUser.rejected().type)
-    expect(end[0].payload).toBe("Can't fetch")
-  });
+  })
 })
