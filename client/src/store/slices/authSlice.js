82051e7ccbb07e70b5472b1cc63defaa7597dcee
Diana Selyuk <selukdiana@gmail.com>
2025-04-30 00:30:23 +0300

    refactor: change login url

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -7,7 +7,7 @@ const initialStateAuth = {
 export const loginUser = createAsyncThunk(
   'auth/loginUser',
   async (userData) => {
-    const response = await fetch('http://localhost:8080/api/auth', {
+    const response = await fetch('http://localhost:8080/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(userData),
